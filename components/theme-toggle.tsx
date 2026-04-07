c5270cf72fff2272b253dfd7f4c7ac983d4c2f6e
Ethan Lee <ejlee168@gmail.com>
2026-04-07 13:22:03 +1000

    remove cursor

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -16,7 +16,7 @@ export function ThemeToggle() {
 
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger render={<Button variant="outline" size="icon" className="cursor-pointer" />}>
+      <DropdownMenuTrigger render={<Button variant="outline" size="icon" />}>
         <FaSun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
         <FaMoon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
         <span className="sr-only">Toggle theme</span>
