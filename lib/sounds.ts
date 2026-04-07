d765065443d09fbff411d04771f9162711b151e0
Ethan Lee <ejlee168@gmail.com>
2026-04-07 22:39:43 +1000

    add error sound

--- lib/sounds.ts
+++ lib/sounds.ts
@@ -2,4 +2,5 @@ export const clicksSprite = {
   click: [0, 400] as [number, number],
   skip: [500, 400] as [number, number],
   wrong: [1000, 400] as [number, number],
+  error: [1500, 500] as [number, number],
 };
