1baaa66d2a676310453af50250db7d1960fb2a9d
Ethan Lee <ejlee168@gmail.com>
2026-04-08 21:57:04 +1000

    prevent cache for diagnoses list

--- lib/queries.ts
+++ lib/queries.ts
@@ -1,7 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
+import { unstable_noStore as noStore } from "next/cache";
 import type { Diagnosis } from "@/lib/types";
 
 export const getDiagnoses = async (): Promise<Diagnosis[]> => {
+  noStore();
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_DEFAULT_KEY!,
