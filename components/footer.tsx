3bce3a62c6fb1c56d8f2fe8590b63ef18d057add
Ethan Lee <ejlee168@gmail.com>
2026-05-16 00:02:21 +1000

    move medical codenames to the footer as an easteregg

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,23 @@
+import { Button } from "./ui/button"
+import { LuGamepad2 } from "react-icons/lu"
+import Link from "next/link"
+
 export default function Footer() {
   return (
-    <footer className="border-t py-6 px-10 text-center text-sm text-muted-foreground">
+    <footer className="relative border-t py-6 px-10 text-center text-sm text-muted-foreground">
       2026 | built by ethan, lachie, mason, bowen, rachel, and jack for the UQMD LaPD Project*
       <br />
       <span className="text-[11px] italic">*educational purposes only</span>
+      <Button
+        variant="ghost"
+        size="icon"
+        className="text-muted-foreground absolute right-4 bottom-4"
+        aria-label={"code-red-link"}
+        data-no-click-sound
+      // onClick={() => router.push("/code-red")}
+      >
+        <Link href="/code-red"><LuGamepad2 /></Link>
+      </Button>
     </footer>
   );
 }
